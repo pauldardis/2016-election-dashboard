b0f44aa1f586dcf72b5db009c64f723aa5bcc198
Ubuntu <ubuntu@ip-172-31-42-184.ec2.internal>
2019-10-11 15:03:53 +0000

    adding extra tables

--- assets/js/graps.js
+++ assets/js/graps.js
@@ -2,20 +2,16 @@ queue()
     .defer(d3.csv, "assets/data/election-2016.csv")
     .await(makeGraphs);
 
-
-
-function makeGraphs(error, salesData) {
-    var ndx = crossfilter(salesData);
-
+function makeGraphs(error, electionData) {
+    var ndx = crossfilter(electionData);
 
 
     // these are the working graphs 
     region_selector(ndx);
     constituency_selector(ndx);
-    elected_selector(ndx);
     party_first_preference_graphs(ndx);
     candidate_graphs(ndx);
-    show_data_table(ndx)
+    show_data_table(ndx);
     show_count_candidate_fg(ndx, "F", "#count-of-women-candidate-fg");
     show_count_candidate_fg(ndx, "M", "#count-of-men-candidate-fg");
     show_count_elected_fg(ndx, "F", "#count-of-women-elected-fg");
@@ -42,25 +38,25 @@ function makeGraphs(error, salesData) {
     show_count_elected_others(ndx, "M", "#count-of-men-elected-others");
 
 
+    dc.renderAll();
+}
+
+queue()
+    .defer(d3.csv, "assets/data/generalelection2016constituencydetails.csv")
+    .await(makeTable);
+
+function makeTable(error, tableData) {
+    var ndx = crossfilter(tableData);
 
 
-
-
-
-    // these are the graphs in test     
-
-    party_total_votes_graphs(ndx);
-    candidate_first_preferance_graphs(ndx);
-    candidate_total_votes_graphs(ndx);
-    candidate_graphs_v2(ndx);
-    // candidate_people(ndx);
-    // show_count_elected_by_gender(ndx, "F", "#count-of-women-elected");
-    // show_count_elected_by_gender(ndx, "M",  "#count-of-men-elected");
-    // show_count_candidate_gender(ndx, "F", "#count-of-women-elected_1");
-    // show_count_candidate_gender(ndx, "M", "#count-of-men-elected_1");
+    // these are the working graphs 
+    show_constituency_table(ndx)
 
     dc.renderAll();
 }
+
+
+
 
 
 // This is the working section 
@@ -85,15 +81,6 @@ function constituency_selector(ndx) {
     group = dim.group();
 
     dc.selectMenu("#constituency-selector")
-        .dimension(dim)
-        .group(group);
-}
-
-function elected_selector(ndx) {
-    var dim = ndx.dimension(dc.pluck('Result'));
-    group = dim.group();
-
-    dc.selectMenu("#elected-selector")
         .dimension(dim)
         .group(group);
 }
@@ -169,7 +156,7 @@ function show_data_table(ndx) {
         ]).sortBy(function(d) {
             return d.Constituency_Name; /* sortBy return = d.Constituency_Name will sort data by Constituency_Names */
         })
-        .order(d3.descending) /* reinsert ; after final peice of this section */
+        .order(d3.ascending) 
 
         /* pagination */
 
@@ -178,9 +165,9 @@ function show_data_table(ndx) {
         .on('pretransition', display);
 
 
-    /* use odd pParty_Abbreviation_v2 size to show the effect better */
+    
     var ofs = 0,
-        pag = 6;
+        pag = 7;
 
     function update_offset() {
         var totFilteredRecs = ndx.groupAll().value();
@@ -464,7 +451,7 @@ function show_count_elected_gp(ndx, gender, element) {
             }
         })
         // .transitionDuration(1500)
-          .formatNumber(d3.format("1.s"))
+        .formatNumber(d3.format("1.s"))
         .group(countThatAreElected)
 }
 
@@ -708,490 +695,28 @@ function show_count_elected_others(ndx, gender, element) {
         .group(countThatAreElected)
 }
 
+function show_constituency_table(ndx) {
 
+    var dim = ndx.dimension(function(d) { return d.dim; });
 
+    var table = dc.dataTable("#dc-constituency-table") /* variable created for pagination */
 
+        .dimension(dim)
+        .group(function(d) { return ""; })
+        .size(Infinity) /* Adjust amount of rows here. Use 'Infinity' to show all data */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//this is the testing section 
-
-
-
-
-function show_count_elected_by_gender(ndx, gender, element) {
-    var countThatAreElected = ndx.groupAll().reduce(
-        function(p, v) {
-            if (v.Gender === gender) {
-                p.count++;
-                if (v.Result === "Elected") {
-                    p.are_elected++;
-                }
-            }
-            return p;
-        },
-        function(p, v) {
-            if (v.Gender === gender) {
-                p.count--;
-                if (v.Result === "Elected") {
-                    p.are_elected--;
-                }
-            }
-            return p;
-        },
-        function() {
-            return { count: 0, are_elected: 0 };
-        },
-    );
-
-
-    dc.numberDisplay(element)
-        // .formatNumber(d3.format("2.s"))
-        .valueAccessor(function(d) {
-            if (d.count == 0) {
-                return 0;
-            }
-            else {
-                return (d.are_elected);
-            }
+        .columns([
+            function(d) { return d.Constituency_Name; },
+            function(d) { return d.Total_Electorate; },
+            function(d) { return ((((parseInt(d.Valid_Poll)) + (parseInt(d.Spoiled))) / (parseInt(d.Total_Electorate)) * 100).toFixed(0)) + "%" },
+            function(d) { return d.Valid_Poll; },
+            function(d) { return d.Spoiled; }
+            
+      
+        ]).sortBy(function(d) {
+            return d.Total_Electorate; /* sortBy return = d.Total_Electorate will sort data by Total_Electorate */
         })
-        .transitionDuration(1500)
-        .group(countThatAreElected)
-}
-
-function show_count_candidate_gender(ndx, gender, element) {
-    var countThatArecandidate = ndx.groupAll().reduce(
-        function(p, v) {
-            if (v.Gender === gender) {
-                p.count++;
-                if (v.Result === "Excluded") {
-                    p.are_excluded++;
-                }
-            }
-            return p;
-        },
-        function(p, v) {
-            if (v.Gender === gender) {
-                p.count--;
-                if (v.Result === "Excluded") {
-                    p.are_excluded--;
-                }
-            }
-            return p;
-        },
-        function() {
-            return { count: 0, are_excluded: 0 };
-        },
-    );
-
-
-    dc.numberDisplay(element)
-        // .formatNumber(d3.format("2.s"))
-        .valueAccessor(function(d) {
-            if (d.count == 0) {
-                return 0;
-            }
-            else {
-                return (d.are_excluded);
-            }
-        })
-        .transitionDuration(1500)
-        .group(countThatArecandidate)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function candidate_people(ndx) {
-//     var dim = ndx.dimension(dc.pluck('Candidate'));
-//     var group = dim.group().reduceSum(dc.pluck('Count_1' ));
-//     var group2 = dim.group().reduceSum(dc.pluck('Result' ));
-
-//     console.log(dim)
-
-//     dc.rowChart("#candidate-people")
-//         .width(600)
-//         .height(8600)
-
-//         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-//         .dimension(dim)
-//         .transitionDuration(1500)
-//         // .elasticX(true)
-//         .group(group,group2);
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function candidate_graphs_v2(ndx) {
-
-    var partyColors = d3.scale.ordinal()
-        .domain(["F.G.", "F.F.", "Others", "S.F.", "LAB.", "G.P."])
-        .range(["pink", "blue", "red", "yellow", "black", "green"]);
-
-    var dim = ndx.dimension(dc.pluck('Party_Abbreviation'));
-    var group = dim.group().reduceSum(dc.pluck('Seat'));
-
-
-    var party_color = ndx.dimension(function(d) {
-        return [d.Party_Abbreviation, d.Seat];
-    });
-
-    var partyGroup = party_color.group();
-
-    // console.log(group.all());
-
-
-    dc.barChart("#candidate-graph-v2")
-        .width(1000)
-        .height(300)
-        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-        .dimension(dim)
-        // .colorAccessor(function(d) {
-        //     return d.key[0];
-        // })
-        .colors(d3.scale.ordinal().range(["#e6550e", "#fd8c3d", "#3182bc", "#fd8c3d", "#fd8c3d", "#fd8c3d", "#fd8c3d"]))
-        .group(group)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .elasticY(true)
-        .xAxisLabel("Region")
-        .yAxis().ticks(10);
-
+        .order(d3.ascending)
 
 }
 
-
-function party_total_votes_graphs(ndx) {
-    var dim = ndx.dimension(dc.pluck('Party_Abbreviation'));
-    var group = dim.group().reduceSum(dc.pluck('Seat'));
-
-    dc.pieChart('#party_total_votes_graphs') /* targeting the div where we want the pie chart*/
-        .height(360)
-        .width(360)
-        .innerRadius(95)
-        .transitionDuration(1500)
-        // .colors(d3.scale.ordinal().range(["#3182bc", "#fd8c3d", "#e6550e"]))
-        .dimension(dim)
-        .renderLabel(true)
-        .legend(dc.legend().x(150).y(130).itemHeight(14).gap(5))
-        // .radius(360)
-        .group(group)
-        .title();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function party_total_votes_graphs(ndx) {
-
-//     var dim = ndx.dimension(dc.pluck('Party_Abbreviation'));
-//     var group = dim.group().reduceSum(dc.pluck('Total_Votes'));
-
-//     dc.pieChart('#party_total_votes_graphs') /* targeting the div where we want the pie chart*/
-//         .height(360)
-//         .width(360)
-//         .innerRadius(95)
-//         .transitionDuration(1500)
-//         // .colors(d3.scale.ordinal().range(["#3182bc", "#fd8c3d", "#e6550e"]))
-//         .dimension(dim)
-//         .renderLabel(true)
-//         .legend(dc.legend().x(110).y(150).itemHeight(13).gap(5))
-//         // .title(function(d) {
-//         //     return d.key + ": " + ((d.value / d3.sum(group.all(),
-//         //         function(d) { return d.value; })) * 100).toFixed(0) + "%";
-//         // })
-//         .on("pretransition", function(chart) 
-//         {
-//             chart.selectAll("text.pie-slice").text(function(d) {
-//                 if (dc.utils.printSingleValue(
-//                         (d.endAngle - d.startAngle) /
-//                         (2 * Math.PI) * 100) >= 6) {
-//                     return dc.utils.printSingleValue(
-//                         (d.endAngle - d.startAngle) /
-//                         (2 * Math.PI) * 100) + "%";
-//                 }
-//             }
-//             )
-//             chart.select("svg")
-//                 .attr("height", "100%")
-//                 .attr("width", "100%")
-//                 .attr("viewBox", "0 0 360 360");
-//             chart.selectAll(".dc-legend-item text")
-//                 .attr("fill", "#1D47FF")
-//                 .text("")
-//                 .append("tspan")
-//                 .text(function(d) { return d.name; })
-//                 .attr("x", 70)
-//                 .attr("text-anchor", "end");
-//         })
-//         .group(group);
-// }
-
-
-
-
-
-
-
-
-
-
-function candidate_first_preferance_graphs(ndx) {
-    var dim = ndx.dimension(dc.pluck('Candidate'));
-    var group = dim.group().reduceSum(dc.pluck('Count_1'));
-
-
-    dc.pieChart('#candidate-first-preferance-graph') /* targeting the div where we want the pie chart*/
-        .height(360)
-        .width(360)
-        .radius(360)
-        .innerRadius(95)
-        .transitionDuration(1500)
-        .dimension(dim)
-        .group(group);
-}
-
-function candidate_total_votes_graphs(ndx) {
-    var dim = ndx.dimension(dc.pluck('Candidate'));
-    var group = dim.group().reduceSum(dc.pluck('Total_Votes'));
-
-
-    dc.pieChart('#candidate-total-votes-graph') /* targeting the div where we want the pie chart*/
-        .height(250)
-        .radius(100)
-        .innerRadius(30)
-        .transitionDuration(1500)
-        .dimension(dim)
-        .group(group);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// Everything under here is in test
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function show_revenue_graphs(ndx) {
-//     var dim = ndx.dimension(dc.pluck('Candidate'));
-//     var group = dim.group().reduceSum(dc.pluck('Total_Votes'));
-//     // var group = dim.group();
-
-//     dc.rowChart("#revenue-graph")
-//         .width(600)
-//         .height(2000)
-//         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-//         .dimension(dim)
-//         .group(group);
-// .transitionDuration(500)
-// .x(d3.scale.ordinal())
-// .xUnits(dc.units.ordinal)
-// .elasticY(true)
-// .xAxisLabel("Region")
-// .yAxis().ticks(10);
-
-
-// }
-
-
-// function candidate_graphs(ndx) {
-//     var dim = ndx.dimension(dc.pluck('Candidate'));
-//     var group = dim.group().reduceSum(dc.pluck('Constituency_Name'));
-
-//     dc.barChart("#candidate-graph")
-//         .width(600)
-//         .height(300)
-//         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-//         .dimension(dim)
-//         .group(group)
-//         .transitionDuration(500)
-//         .x(d3.scale.ordinal())
-//         .xUnits(dc.units.ordinal)
-//         .elasticY(true)
-//         .xAxisLabel("Region")
-//         .yAxis().ticks(10);
-
-// }
